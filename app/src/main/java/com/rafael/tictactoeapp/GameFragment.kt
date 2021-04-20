e6cf71678faf07a80b3fc753895f56f7d3a9fee7
rafael-cagliari <rafael.cagliari22@gmail.com>
2021-04-19 22:08:48 -0300

    -implemented the logic for the game fragment score board -implemented shared view model

--- app/src/main/java/com/rafael/tictactoeapp/GameFragment.kt
+++ app/src/main/java/com/rafael/tictactoeapp/GameFragment.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.rafael.tictactoeapp.databinding.GameFragmentBinding
 import com.rafael.tictactoeapp.viewmodel.TicTacViewModel
 
 
 class GameFragment : Fragment() {
 
-    private val ticTacViewModel: TicTacViewModel by viewModels()
+    private lateinit var ticTacViewModel: TicTacViewModel
 
     var binding:GameFragmentBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,13 +28,15 @@ class GameFragment : Fragment() {
         // Inflate the layout for this fragment
         val fragmentBinding = GameFragmentBinding.inflate(inflater, container, false)
         binding = fragmentBinding
+        ticTacViewModel = ViewModelProvider(requireActivity()).get(TicTacViewModel::class.java)
         return fragmentBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-     binding!!.scoreBoard.text= "oie"
-        Toast.makeText(context, ticTacViewModel.players.value?.get(0)?.name, Toast.LENGTH_SHORT).show()
+        ticTacViewModel.players.observe(viewLifecycleOwner, Observer { binding!!.apply {
+            scoreBoardPlayer1.text=ticTacViewModel.players.value?.get(0)?.name
+            scoreBoardScores.text=" ${ticTacViewModel.players.value?.get(0)?.score}  X  ${ticTacViewModel.players.value?.get(1)?.score} "
+            scoreBoardPlayer2.text=ticTacViewModel.players.value?.get(1)?.name}})
     }
-
-    }
+}
--- app/src/main/java/com/rafael/tictactoeapp/PlayerNameFragment.kt
+++ app/src/main/java/com/rafael/tictactoeapp/PlayerNameFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.rafael.tictactoeapp.databinding.PlayerNameFragmentBinding
 import com.rafael.tictactoeapp.viewmodel.TicTacViewModel
@@ -14,7 +13,6 @@ import com.rafael.tictactoeapp.viewmodel.TicTacViewModel
 
 class PlayerNameFragment : Fragment() {
 
-    private val ticTacViewModel: TicTacViewModel by viewModels()
 
     var binding: PlayerNameFragmentBinding? = null
 
@@ -33,10 +31,9 @@ class PlayerNameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding?.button?.text = "Start Game"
-
+        val ticTacViewModel = ViewModelProvider(requireActivity()).get(TicTacViewModel::class.java)
         //once the start button is pressed, player 1 and player 2 are created, receiving the names typed on the boxes
         binding?.button?.setOnClickListener { ticTacViewModel.createPlayers(binding!!.player1Name.text.toString(), binding!!.player2Name.text.toString())
         findNavController().navigate(PlayerNameFragmentDirections.actionPlayerNameFragmentToGameFragment())
-            Toast.makeText(context, ticTacViewModel.players.value?.get(0)?.name, Toast.LENGTH_SHORT).show()
     }
 }}
